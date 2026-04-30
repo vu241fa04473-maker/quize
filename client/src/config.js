49d7a8a0e579eff668bace48932b647fb1d43410
vu241 <vu241@example.com>
2026-04-30 22:08:34 +0530

    Update frontend to point to new Render backend (quize-6)

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 axios.defaults.headers.common['Bypass-Tunnel-Reminder'] = 'true';
 
-const API_BASE_URL = 'https://quize-i74t.onrender.com/api';
+const API_BASE_URL = 'https://quize-6.onrender.com/api';
 
 export default API_BASE_URL;
